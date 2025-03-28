b045748bdbf2a6b3dc84b15ee874d7748bcf49e2
Lucas Resende <lucasalves2180@gmail.com>
2025-03-27 21:38:26 -0300

    excluindo pasta antiga

--- alugueldecarros/src/main/java/com/todecarro/alugueldecarros/repository/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.todecarro.alugueldecarros.repository;
-
-import com.todecarro.alugueldecarros.models.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
